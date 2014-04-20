ba4673b121815a949219f73e4de86eb1d88db6e5
kitaev-chen <kitaev@126.com>
2014-04-20 21:50:06 +0800

    modified knitpost

--- _drafts/KnitPost.R
+++ _drafts/KnitPost.R
@@ -5,5 +5,16 @@ KnitPost <- function(input, base.url = "/") {
     opts_chunk$set(fig.path = fig.path)
     opts_chunk$set(fig.cap = "center")
     render_jekyll()
-    knit(input, output = "_posts/" , envir = parent.frame())
+   
+    #knitoutput.path <- paste0("_posts/", sub(".Rmd$", "", basename(input)), "/")
+    #knit(input, output = "_posts" , envir = parent.frame())
+    
+    md.path <- paste0("_posts/", sub(".Rmd$", "", basename(input)), ".md")
+    md_file <- knit(input, output = md.path , envir = parent.frame())
+    
+    #html.path <- paste0("_posts/", sub(".Rmd$", "", basename(input)), ".html")  
+    #html_file <- knit2html(input, output = html.path , envir = parent.frame())
+    #html_file <- paste(file_path_sans_ext(md_file), '.html', sep = '')
+    
+    #markdownToHTML(md_file, html_file)    
 }
